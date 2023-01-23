ccc39ce28dc3c6e798988774e2b099c5ff39af74
Daouadji Aymen <37842353+daouadji506@users.noreply.github.com>
2023-01-23 23:05:14 +0100

    ✅ update AppContainer

--- src/renderer/ui/containers/app_container/AppContainer.tsx
+++ src/renderer/ui/containers/app_container/AppContainer.tsx
@@ -1,5 +1,21 @@
+import MainLogo from '@components/main_logo';
 import './style/index.scss';
+import IpDisplay from '@components/ip_display';
+import MainButton from '@components/main_button';
+import ServicesStatus from '@components/services_status';
 interface AppContainerProps {}
 export default function AppContainer({}: AppContainerProps) {
-  return <div className="app-container"></div>;
+  return (
+    <div className="app-container">
+      <MainLogo />
+      <div className="middle-wrapper">
+        <IpDisplay ipAdr="0.0.0.0" />
+        <MainButton state="start" />
+      </div>
+      <div className="services-container">
+        <ServicesStatus name="Clinicord services" state="stopped" />
+        <ServicesStatus name="Clinicord services" state="stopped" />
+      </div>
+    </div>
+  );
 }
