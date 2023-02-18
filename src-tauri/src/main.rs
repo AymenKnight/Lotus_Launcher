15d13857fc4a093a6b288eceb06f1ae6d1b120ba
danzo7 <aymenmiro@gmail.com>
2023-02-18 02:47:54 +0100

    ✨ Add logic and functionalities. - Handle servers excution. - improve backend logic. - add tray icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,77 +2,164 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-use sysinfo::{ProcessExt, System, SystemExt};
-use tauri::{Manager, WindowEvent};
+
+use std::fs;
+use std::sync::Mutex;
+
 use local_ip_address::local_ip;
+use sysinfo::{ProcessExt, System, SystemExt};
+use tauri::{
+    api::dialog::{ask, MessageDialogBuilder, MessageDialogButtons},
+    CustomMenuItem, Manager, State, SystemTray, SystemTrayEvent, SystemTrayMenu,
+};
+
+struct ServerState(Mutex<String>);
+
 #[tauri::command]
 fn kill_api() -> String {
-    let mut found=false;
+    let mut found = false;
     let binding = System::new_all();
-for process in binding.processes_by_name("exec.mc"){
-process.kill().then(|| println!("Killed process {}", process.name())).expect("Failed to kill process ");
-found=true;
-}
+    for process in binding.processes_by_name("exec.mc") {
+        process
+            .kill()
+            .then(|| println!("Killed process {}", process.name()))
+            .expect("Failed to kill process ");
+        found = true;
+    }
 
-if found==false{
-    format!("API is not running or already closed")
-}
-else{
-    format!("API closed successfully")}
+    if found == false {
+        format!("API is not running or already closed")
+    } else {
+        format!("API closed successfully")
+    }
 }
 #[tauri::command]
 fn is_api_running() -> String {
-    let mut found=false;
+    let mut found = false;
     let binding = System::new_all();
-for _process in binding.processes_by_name("exec.mc"){
-found=true;
-break;
-}
+    for _process in binding.processes_by_name("exec.mc") {
+        found = true;
+        break;
+    }
 
-if found==false{
-    format!("false")
+    if found == false {
+        format!("false")
+    } else {
+        format!("true")
+    }
 }
-else{
-    format!("true")}
-}
-
 
 #[tauri::command]
 fn get_ip() -> String {
-    let my_local_ip = local_ip().unwrap();
+    let my_local_ip = local_ip().unwrap(); //TODO: handle error if no ip found
     format!("{:?}", my_local_ip)
 }
-// #[tauri::command]
-// fn init_process( window: Window) {
-//     // let command :StdCommand= StdCommand::from(
-//     //     Command::new_sidecar("start")
-//     //         .expect("Failed to start api"),
-//     //     );command.spawn().expect("Failed to spawn `backend_server` binary");
-//     //     command.stdin()
-// //   std::thread::spawn(move || {
-// //    let c= Command::new("start");
-// //  let child=  c.spawn();
-// //  child.unwrap().0.
-// //   });
-// }
+#[tauri::command]
+fn is_bd_initialized(handle: tauri::AppHandle) -> String {
+    let resource_path = handle
+        .path_resolver()
+        .resolve_resource("Binaries/ThirdParty/PostgreSQL/data/")
+        .expect("failed to resolve resource");
+    let is_exist = resource_path.exists();
+    if !is_exist {
+        fs::create_dir(resource_path).expect("failed to create directory");
+    }
+    format!("{:?}", is_exist)
+}
+
+#[tauri::command]
+fn set_server_state(state: String, old_state: State<ServerState>) -> String {
+    let mut server_state = old_state.0.lock().unwrap();
+    *server_state = state;
+    format!("{}", *server_state)
+}
 
 fn main() {
-
-    tauri::Builder::default().setup(
-        |app| {
-            let main_window = app.get_window("main").unwrap();
-
-            Ok(())}
-    
-        ) .on_window_event(move |event| match event.event() {
-            WindowEvent::Destroyed =>  {
-              println!("Window destroyed");
-              kill_api();
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit Lotus Launcher");
+    let tray_menu = SystemTrayMenu::new().add_item(quit);
+    tauri::Builder::default()
+        .manage(ServerState(Default::default()))
+        .on_window_event(move |event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                let window = event.window();
+                let state = window.state::<ServerState>().0.lock().unwrap().to_string();
+                api.prevent_close();
+                if state != "started" {
+                    ask(
+                        Some(&window),
+                        "Close Lotus Launcher ".to_owned(),
+                        "Are you sure you want to close Lotus Launcher?",
+                        move |answer| {
+                            if answer {
+                                std::process::exit(0);
+                            }
+                        },
+                    );
+                } else {
+                    event.window().hide().unwrap();
+                }
             }
-        
+
             _ => {}
-          })
-        .invoke_handler(tauri::generate_handler![kill_api,is_api_running,get_ip])
+        })
+        .system_tray(SystemTray::new().with_menu(tray_menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::DoubleClick {
+                position: _,
+                size: _,
+                ..
+            } => {
+                let window = app.get_window("main").unwrap();
+                window.show().unwrap();
+                window.set_focus().unwrap();
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => {
+                let window = app.get_window("main").unwrap();
+                //get state
+                let state = app.state::<ServerState>().0.lock().unwrap().to_string();
+
+                match id.as_str() {
+                    "quit" => {
+                        if state == "started" {
+                            MessageDialogBuilder::new(
+                                "Lotus Launcher is running",
+                                "Please stop the server before closing Lotus Launcher",
+                            )
+                            .buttons(MessageDialogButtons::Ok)
+                            .parent(&window)
+                            .kind(tauri::api::dialog::MessageDialogKind::Warning)
+                            .show(move |_| {
+                                window.show().unwrap();
+                                window.set_focus().unwrap();
+                            });
+
+                            //emit event
+                        } else {
+                            ask(
+                                Some(&window),
+                                "Close Lotus Launcher ".to_owned(),
+                                "Are you sure you want to close Lotus Launcher?",
+                                move |answer| {
+                                    if answer {
+                                        std::process::exit(0);
+                                    }
+                                },
+                            );
+                        }
+                    }
+
+                    _ => {}
+                }
+            }
+            _ => {}
+        })
+        .invoke_handler(tauri::generate_handler![
+            kill_api,
+            is_api_running,
+            get_ip,
+            is_bd_initialized,
+            set_server_state
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/renderer/ui/containers/app_container/AppContainer.tsx
+++ src/renderer/ui/containers/app_container/AppContainer.tsx
@@ -3,31 +3,66 @@ import './style/index.scss';
 import IpDisplay from '@components/ip_display';
 import MainButton from '@components/main_button';
 import ServicesStatus from '@components/services_status';
-import { useServerState, useServicesStore } from '@stores/servicesStore';
+import { useApiServer, useApiServerStore } from '@stores/apiServerStore';
+import { useDBServerStore } from '@stores/dbServerStore';
+import { useEffect } from 'react';
+import { ask } from '@tauri-apps/api/dialog';
 
 interface AppContainerProps {}
 export default function AppContainer({}: AppContainerProps) {
-  const { serverState, serverIp } = useServerState();
-
+  const { serverState, serverIp } = useApiServer();
+  const { serverState: dbState, error } = useDBServerStore();
+  const isTauri = !!(window as any).window.__TAURI_IPC__;
+  useEffect(() => {
+    const preventRefresh = (e: KeyboardEvent) => {
+      if (e.key === 'F5'|| (e.ctrlKey && e.key === 'r')) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    };
+    window.addEventListener('keydown', preventRefresh, false);
+    return () => {
+      window.removeEventListener('keydown', preventRefresh, false);
+    }
+  }, [])
+  
   return (
     <div className="app-container">
       <MainLogo />
-      <div className="middle-wrapper">
-        {serverIp && <IpDisplay ipAdr={serverIp} />}
-        <MainButton
-          state={serverState}
-          onPress={() => {
-            if (serverState === 'stopped')
-              useServicesStore.getState().startServer();
-            if (serverState === 'started')
-              useServicesStore.getState().stopServer();
-          }}
-        />
-      </div>
-      <div className="services-container">
-        <ServicesStatus name="Database services" state="stopped" />
-        <ServicesStatus name="Clinicord services" state={serverState} />
-      </div>
+      {isTauri ? (
+        <>
+          <div className="middle-wrapper">
+            {serverIp && <IpDisplay ipAdr={serverIp} />}
+            <MainButton
+              state={dbState=="started"?serverState=="stopped"?"starting": serverState:dbState}
+              onPress={async () => {
+                if(dbState === 'started') {  
+                  const confirmed = await ask('Are you sure you want to stop the server?',{title:"Stop the server",type:"warning"});
+                  if(!confirmed) return;
+              
+                  if(serverState === 'stopped')
+                    useApiServerStore.getState().startServer();
+                  else  useDBServerStore.getState().stopServer();
+                }
+                else  {
+                  useDBServerStore.getState().startServer();
+                }
+                
+              }}
+            />
+          </div>
+          <div className="services-container">
+            <ServicesStatus name="Database services" state={dbState} />
+            <ServicesStatus name="Clinicord services" state={serverState} />
+          </div>
+        </>
+      ) : (
+        <span>
+          The dev server is running. You only can access it through the tauri
+          app.
+        </span>
+      )}
+      {error && <span>{error}</span>}
     </div>
   );
 }
